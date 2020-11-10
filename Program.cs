d5044e3460ad592b2f21f1b61cd2f0c71c1f815c
Thomas Haller <Thomas.Haller@lab10.coop>
2020-11-10 09:57:56 +0100

    hello world

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dmd_vision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
